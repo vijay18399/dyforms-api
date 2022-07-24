f2b578255ca12adc2f09d7faa3cb0e505ab75017
vijay18399 <vijayreddy18399@gmail.com>
2022-07-24 11:37:46 +0530

    moved from sqlite to postgres

--- util/db.js
+++ util/db.js
@@ -1,7 +1,13 @@
+require("dotenv/config");
 const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "database.sqlite",
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 module.exports = sequelize;
